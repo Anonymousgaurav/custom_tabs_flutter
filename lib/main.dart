9a2f58a1662636a9b2593af5d2e107d8ed9b9460
Gaurav Kumar <gauravswarankar1@gmail.com>
2024-08-10 10:43:06 +0530

    tab_bar_3

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:custom_tab_flutter/tab_bar_1.dart';
 import 'package:custom_tab_flutter/tab_bar_2.dart';
+import 'package:custom_tab_flutter/tab_bar_3.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home:  SafeArea(child: Material(child: CustomTabPage(),))
+      home:  SafeArea(child: Material(child: CustomTabBarsPage(),))
     );
   }
 }
